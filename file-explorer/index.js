81f31b4268ed5d1bb24e67d839eb446d1d08495f
HBC <qifeng0748@vip.qq.com>
2015-08-19 08:59:45 +0800

    完成
    文件浏览

--- file-explorer/index.js
+++ file-explorer/index.js
@@ -1,3 +1,76 @@
 /**
  * Created by HG on 15/08/18.
  */
+var fs = require('fs'),
+    stdin = process.stdin,
+    stdout = process.stdout,
+    stats = [];
+
+fs.readdir(process.cwd(), function(err, files) {
+    console.log('');
+
+    if(!files.length) {
+        console.log('    \033[31m No files to show!\033[39m\n');
+    }
+
+    console.log('Select which files or directiory you want see\n');
+
+    function file(i) {
+        var filename = files[i];
+
+        fs.stat(__dirname + '/' + filename, function(err, stat) {
+            stats[i] = stat;
+            if(stat.isDirectory()) {
+                console.log('    ' + i + ' \033[36m' + filename + '\033[39m');
+            } else {
+                console.log('    ' + i + ' \033[90m' + filename + '\033[39m');
+            }
+
+            i++;
+
+            if(i == files.length) {
+                read();
+            } else {
+                file(i);
+            }
+        });
+    }
+
+    function read() {
+        console.log('');
+        stdout.write('    \033[33mEnter your choice: \033[39m');
+        stdin.resume();
+        stdin.setEncoding('utf8');
+        stdin.on('data', option);
+    }
+
+    function option(data) {
+        data = Number(data);
+        var filename = files[data];
+
+        if(!filename) {
+            stdout.write('    \033[39mEnter your choice: \033[39m');
+        } else {
+            stdin.pause();
+            if(stats[data].isDirectory()) {
+                fs.readdir(__dirname + '/' + filename, function(err, files) {
+                    console.log('');
+                    console.log('    (' + files.length + ' files)');
+
+                    files.forEach(function(file) {
+                        console.log('        -    ' + file);
+                    });
+
+                    console.log('');
+                })
+            } else {
+                fs.readFile(__dirname + '/' + filename, 'utf8', function(err, data) {
+                    console.log('');
+                    console.log('\033[90m' + data.replace(/(.*)/g, '    $1') + '\033[39m');
+                });
+            }
+        }
+    }
+
+    file(0);
+});
